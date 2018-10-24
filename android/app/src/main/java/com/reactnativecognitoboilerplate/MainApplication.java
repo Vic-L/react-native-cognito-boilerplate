bfea79b36a335fd1e94033247c7babda5c441d10
Vic-L <vljc17@gmail.com>
2018-10-24 18:20:28 +0700

    chore: install react-native-splash-screen and link

--- android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativecognitoboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new RNAWSCognitoPackage(),
             new ReactNativeConfigPackage()
       );
