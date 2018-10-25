837cdd9a3e2eb34fd9a9931bc311df4289b604e4
Vic-L <vljc17@gmail.com>
2018-10-25 17:51:11 +0700

    fix: remove extra duplicate package

--- android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
@@ -31,7 +31,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
             new PickerPackage(),
             new SplashScreenReactPackage(),
             new RNAWSCognitoPackage(),
