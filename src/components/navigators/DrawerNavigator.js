8856c525c18b37df61cd9c4dc8f98cbb8f121e61
Vic-L <vljc17@gmail.com>
2018-10-29 13:24:54 +0800

    feat: use Dimensions to set exact drawerWidth

--- src/components/navigators/DrawerNavigator.js
+++ src/components/navigators/DrawerNavigator.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Image } from 'react-native'
+import {
+  Image,
+  Dimensions,
+} from 'react-native'
 import {
   createDrawerNavigator,
 } from 'react-navigation'
@@ -9,7 +12,7 @@ import StackNavigator from './StackNavigator'
 import Products from '../screens/Products'
 
 const DrawerNavigatorConfig = {
-  drawerWidth: 200, // - Width of the drawer or a function returning it.
+  drawerWidth: Dimensions.get('window').width * 0.85, // - Width of the drawer or a function returning it.
   drawerPosition: 'left', // - Options are left or right. Default is left position.
   // contentComponent, // - Component used to render the content of the drawer, for example, navigation items. Receives the navigation prop for the drawer. Defaults to DrawerItems. For more information, see below.
   // contentOptions, // - Configure the drawer content, see below.
