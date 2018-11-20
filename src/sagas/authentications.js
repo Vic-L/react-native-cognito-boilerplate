4284d6bfe67a106154f822717fc6ae76d95f2f56
Vic-L <vljc17@gmail.com>
2018-11-20 19:42:37 +1000

    feat: handle first time login, different user login and same user login scenarios for asking to setup biometric

--- src/sagas/authentications.js
+++ src/sagas/authentications.js
@@ -1,6 +1,8 @@
+import _ from 'lodash'
 import * as Keychain from 'react-native-keychain'
 import DeviceInfo from 'react-native-device-info'
-import { put, call, takeLatest } from 'redux-saga/effects'
+import { put, call, take, takeLatest } from 'redux-saga/effects'
+import { eventChannel } from 'redux-saga'
 import { AsyncStorage, Alert } from 'react-native'
 
 export default function* authenticationsSaga() {
@@ -8,17 +10,25 @@ export default function* authenticationsSaga() {
 }
 
 function* saveCredentialsToKeychain({ email, password }) {
+  let previousEmail = null
   try {
-    // Store email only in Async Storage
-    yield call([AsyncStorage, 'setItem'], `${DeviceInfo.getBundleId()}:email`, email)
-    // Store the credentials in keychain
-    yield call(Keychain.setGenericPassword, email, password, {
-      accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY_OR_DEVICE_PASSCODE,
-      accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED,
-      service: DeviceInfo.getBundleId()
-    })
-    // TODO request touchId
-    yield put({type: 'SAVED_CREDENTIALS_TO_KEYCHAIN'})
+    previousEmail = yield call([AsyncStorage, 'getItem'], `${DeviceInfo.getBundleId()}:email`)
+
+    // same user sign in again
+    if (previousEmail === email) {
+      // do nothing
+    } else if (_.isNull(previousEmail)) {
+      // previous email is null, so nobody sign in before
+      yield call(promptBiometricSetup, email, password)
+    } else {
+      // different user sign in; need to remove whatever is present in keychain
+      yield call([Keychain, 'resetGenericPassword'],{
+        service: DeviceInfo.getBundleId()
+      })
+
+      // prompt to setup biometric
+      yield call(promptBiometricSetup, email, password)
+    }
   } catch (err) {
     Alert.alert(
       'Alert',
@@ -26,4 +36,55 @@ function* saveCredentialsToKeychain({ email, password }) {
       [{text: 'OK'}]
     )
   }
+  
+  // anyway, store only email in AsyncStorage
+  try {
+    yield call([AsyncStorage, 'setItem'], `${DeviceInfo.getBundleId()}:email`, email)
+
+  } catch (err) {
+    Alert.alert(
+      'Alert',
+      err.message,
+      [{text: 'OK'}]
+    )
+  }
+}
+
+function* promptBiometricSetup(email, password) {
+  const biometricSupported = yield call(Keychain.getSupportedBiometryType)
+  if (biometricSupported) {
+    const channel = yield call(requestSetupBiometricPermission, biometricSupported)
+    const actionToTake = yield take(channel) // block saga until there is message from channel
+    if (actionToTake === 'SETUP_BIOMETRIC') {
+      yield call(setupBiometric, email, password)
+    }
+  } else {
+    console.log('Biometric not supported')
+  }
+}
+
+function requestSetupBiometricPermission(biometricType) {
+  return eventChannel(emitter => {
+    Alert.alert(
+      'Setup Biometric?',
+      `For easier sign in using ${biometricType}`,
+      [
+        {text: 'No', onPress: () => emitter('NO_SETUP_BIOMETRIC')},
+        {text: 'Yes', onPress: () => emitter('SETUP_BIOMETRIC')},
+      ],
+      { cancelable: false }
+    )
+
+    const unsubscribe = () => {}
+    return unsubscribe
+  })
+}
+
+function* setupBiometric(email, password) {
+  // store the credentials in keychain
+  yield call([Keychain, 'setGenericPassword'], email, password, {
+    accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY_OR_DEVICE_PASSCODE,
+    accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED,
+    service: DeviceInfo.getBundleId()
+  })
 }
