836e632a55fbddfb270ef035721015a492206f78
Vic-L <vljc17@gmail.com>
2018-11-20 11:02:36 +1000

    fix: remove TouchID related implementation after login

--- src/components/screens/app/Main.js
+++ src/components/screens/app/Main.js
@@ -4,66 +4,11 @@ import {
   AsyncStorage,
   Alert,
 } from 'react-native'
-import TouchID from 'react-native-touch-id'
 import Auth from '@aws-amplify/auth'
 
 import Button from '../../elements/Button'
 
 class Main extends React.Component {
-  async componentDidMount() {
-    try {
-      // use non unified errors which is the default
-      const touchIDSupported = await TouchID.isSupported()
-
-      if (touchIDSupported) {
-        // ask to setup touchID
-      } else {
-        // do nothing
-        Alert.alert(
-          'TouchID Not Supported Alert',
-          JSON.stringify(touchIDSupported),
-          [{text: 'OK'}],
-          { cancelable: false }
-        )
-      }
-    } catch (err) {
-      // handle unified TouchID errors
-      if (err.name === 'TouchIDError') {
-        switch(err.code) {
-          case 'AUTHENTICATION_FAILED':
-          case 'USER_CANCELED':
-          case 'SYSTEM_CANCELED':
-          case 'NOT_PRESENT':
-          case 'NOT_SUPPORTED':
-          case 'NOT_AVAILABLE':
-          case 'NOT_ENROLLED':
-          case 'TIMEOUT':
-          case 'LOCKOUT':
-          case 'LOCKOUT_PERMANENT':
-          case 'PROCESSING_ERROR':
-          case 'USER_FALLBACK':
-          case 'FALLBACK_NOT_ENROLLED':
-          case 'UNKNOWN_ERROR':
-          default:
-            Alert.alert(
-              'TouchID Alert',
-              JSON.stringify(err),
-              [{text: 'OK'}],
-              { cancelable: false }
-            )
-        }
-      // handle other errors (Async Storage errors)
-      } else {
-        Alert.alert(
-          'Alert',
-          err.message,
-          [{text: 'OK'}],
-          { cancelable: false }
-        )
-      }
-    }
-  }
-
   render() {
     return (
       <View style={{
