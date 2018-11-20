e219672f83863bda749b5ddc5c9979d80ac93010
Vic-L <vljc17@gmail.com>
2018-11-20 14:05:55 +1000

    feat: link react-native-device-info and react-native-app-settings on android

--- android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
@@ -3,6 +3,8 @@ package com.reactnativecognitoboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.krazylabs.OpenAppSettingsPackage;
 import com.oblador.keychain.KeychainPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -32,6 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
+            new OpenAppSettingsPackage(),
             new KeychainPackage(),
             new PickerPackage(),
             new SplashScreenReactPackage(),
