533a6f52a087572af788c7edb3887adc337f9db1
Vic-L <vljc17@gmail.com>
2019-01-22 10:42:41 +0900

    feat: refetch example

--- src/components/screens/Products.js
+++ src/components/screens/Products.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
+  Alert,
   Text,
 } from 'react-native';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
+import ButtonWithLoader from '../elements/ButtonWithLoader';
 
 const GET_PRODUCTS_QUERY = gql`
   query getProducts {
@@ -22,16 +24,70 @@ const Wrapper = styled.View`
 `;
 
 class Products extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.refetchButton = React.createRef();
+  }
+
+  requestRefetch(refetch) {
+    Alert.alert(
+      '',
+      'Are you sure?',
+      [
+        { text: 'No' },
+        {
+          text: 'Yes',
+          onPress: refetch
+        },
+      ]
+    );
+  }
+
   render() {
     return (
       <Wrapper>
         <Text>Products</Text>
         <Query query={GET_PRODUCTS_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) return <Text>Loading...</Text>;
-            if (error) return <Text>Error! {error.message}</Text>;
+          {({ loading, error, data, refetch }) => {
+            const { current: button } = this.refetchButton;
 
-            return data.allProducts.map((product) => <Text key={product.id}>{product.name}</Text>);
+            if (loading) {
+              if (button) {
+                button.load();
+              }
+              return <Text>Loading...</Text>;
+            }
+            if (error) {
+              if (button) {
+                button.reset();
+              }
+              return <Text>Error! {error.message}</Text>;
+            }
+
+            if (button) {
+              button.reset();
+            }
+
+            const productsList = data.allProducts.map((product) => 
+              <Text key={product.id}>{product.name}</Text>
+            );
+
+            return (
+              <React.Fragment>
+                <ButtonWithLoader
+                  label='Refetch'
+                  style={{
+                    alignSelf: 'center',
+                  }}
+                  noBorder
+                  expandOnFinish
+                  ref={this.refetchButton}
+                  onPress={this.requestRefetch.bind(this, refetch)}
+                />
+                {productsList}
+              </React.Fragment>
+            );
           }}
         </Query>
       </Wrapper>
