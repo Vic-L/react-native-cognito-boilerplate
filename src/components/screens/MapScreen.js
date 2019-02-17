05623800758f337418f253f7a63407eb2ad491d0
Vic-L <vljc17@gmail.com>
2019-02-18 00:05:19 +0900

    feat: implement react-native-maps with google maps

--- src/components/screens/MapScreen.js
+++ src/components/screens/MapScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const Wrapper = styled.View`
   flex: 1;
@@ -7,7 +8,19 @@ const Wrapper = styled.View`
 `;
 
 const MapScreen = () => (
-  <Wrapper />
+  <Wrapper>
+    <MapView
+      style={{ flex: 1 }}
+      provider={PROVIDER_GOOGLE}
+      initialRegion={{
+        latitude: 1.3521,
+        longitude: 103.8198,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      showsUserLocation
+    />
+  </Wrapper>
 );
 
 export default MapScreen;
