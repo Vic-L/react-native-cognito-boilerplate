89bf69cac1e7eaa88f6d195e5006fc34c282e986
Vic-L <vljc17@gmail.com>
2019-02-17 18:07:42 +0900

    feat: add logout button to drawer

--- src/components/elements/DrawerContentComponent.js
+++ src/components/elements/DrawerContentComponent.js
@@ -1,24 +1,63 @@
 import React from 'react';
 import {
+  Alert,
   Text,
   ScrollView,
 } from 'react-native';
 import {
   DrawerItems,
 } from 'react-navigation';
+import Auth from '@aws-amplify/auth';
 import styled from 'styled-components';
+
+import Button from './Button';
 
 const Wrapper = styled.SafeAreaView`
   flex: 1;
 `;
 
-const DrawerContentComponent = props => (
-  <ScrollView>
-    <Wrapper forceInset={{ top: 'always', horizontal: 'never' }}>
-      <Text>App Title</Text>
-      <DrawerItems {...props} />
-    </Wrapper>
-  </ScrollView>
-);
+class DrawerContentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onLogout = this.onLogout.bind(this);
+  }
+
+  async onLogout() {
+    const { navigation } = this.props;
+    try {
+      await Auth.signOut();
+      navigation.navigate('Welcome');
+    } catch (err) {
+      console.log(err);
+      Alert.alert(
+        'Alert',
+        err.message || err,
+        [{ text: 'OK' }],
+      );
+    }
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Wrapper forceInset={{
+          top: 'always',
+          horizontal: 'never',
+        }}
+        >
+          <Text>App Title</Text>
+
+          <DrawerItems {...this.props} />
+
+          <Button
+            text="LOGOUT"
+            onPress={this.onLogout}
+          />
+        </Wrapper>
+      </ScrollView>
+    );
+  }
+}
 
 export default DrawerContentComponent;
