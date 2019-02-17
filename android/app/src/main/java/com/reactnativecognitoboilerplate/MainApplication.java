c06e71ceaa0f154e24b964bf6aab01c5f2f8f5ab
Vic-L <vljc17@gmail.com>
2019-02-17 22:09:23 +0900

    chore: react-navigation v3

--- android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativecognitoboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new RNFetchBlobPackage(),
             new VectorIconsPackage(),
             new ReactNativeConfigPackage(),
