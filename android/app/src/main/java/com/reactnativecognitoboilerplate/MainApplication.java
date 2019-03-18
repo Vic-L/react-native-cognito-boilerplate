af19e147919b6a4e263fdfdbe48490cb9eaeb81e
Vic-L <vljc17@gmail.com>
2019-03-18 13:49:12 -0800

    chore: react-native-date-picker

--- android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativecognitoboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativecognitoboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.henninghall.date_picker.DatePickerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new DatePickerPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage(),
             new RNFetchBlobPackage(),
